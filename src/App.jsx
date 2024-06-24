1a02cc0dd5baa96c702d1c28b38db6a866bfb6d7
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-06-24 12:14:55 -0300

    se guardan estados en localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,95 @@
+// import "./App.css";
+// import { useState } from "react";
+// import ListarCompras from "./components/ListaCompras";
+// import ComprasForm from "./components/ComprasForm";
+
+// function App() {
+//   const [product, setProduct] = useState([]);
+//   const [total, setTotal] = useState(0);
+
+//   const onCompleted = (id) => {
+//     setProduct(
+//       product.map((producto) =>
+//         producto.id === Number(id)
+//           ? { ...producto, completed: !producto.completed }
+//           : producto
+//       )
+//     );
+//   };
+
+//   const onDeleteProduct = (id) => {
+//     setProduct(product.filter((producto) => producto.id !== id));
+
+//     const newTotal = product
+//       .filter((producto) => producto.id !== id)
+//       .reduce((acc, prod) => acc + (prod.valor || 0), 0);
+//     setTotal(newTotal);
+//   };
+
+//   const addProduct = (nuevoProducto) => {
+//     const newProduct = {
+//       id: +new Date(),
+//       producto: nuevoProducto,
+//       completed: false,
+//       valor: 0,
+//     };
+//     setProduct([...product, newProduct]);
+//   };
+
+//   const handleValorChange = (id, newValor) => {
+//     const updatedProducts = product.map((producto) => {
+//       if (producto.id === id) {
+//         return { ...producto, valor: newValor };
+//       }
+//       return producto;
+//     });
+//     setProduct(updatedProducts);
+//     const newTotal = updatedProducts.reduce(
+//       (acc, prod) => acc + (prod.valor || 0),
+//       0
+//     );
+//     setTotal(newTotal);
+//   };
+
+//   return (
+//     <>
+//       <h1 className="text-3xl font-bold pb-2 ">Lista de compras</h1>
+//       <ComprasForm addTask={addProduct} />
+//       <ListarCompras
+//         product={product}
+//         onCompleted={onCompleted}
+//         onDeleteProduct={onDeleteProduct}
+//         onValorChange={handleValorChange}
+//       />
+//       <p>Total: {total}</p>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ListarCompras from "./components/ListaCompras";
 import ComprasForm from "./components/ComprasForm";
 
 function App() {
-  const [product, setProduct] = useState([]);
-  const [total, setTotal] = useState(0);
+  const [product, setProduct] = useState(() => {
+    const savedProducts = localStorage.getItem("products");
+    return savedProducts ? JSON.parse(savedProducts) : [];
+  });
+
+  const [total, setTotal] = useState(() => {
+    const savedTotal = localStorage.getItem("total");
+    return savedTotal ? JSON.parse(savedTotal) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(product));
+    const newTotal = product.reduce((acc, prod) => acc + (prod.valor || 0), 0);
+    setTotal(newTotal);
+    localStorage.setItem("total", JSON.stringify(newTotal));
+  }, [product]);
 
   const onCompleted = (id) => {
     setProduct(
@@ -19,11 +103,6 @@ function App() {
 
   const onDeleteProduct = (id) => {
     setProduct(product.filter((producto) => producto.id !== id));
-    // Recalcular el total después de eliminar un producto
-    const newTotal = product
-      .filter((producto) => producto.id !== id)
-      .reduce((acc, prod) => acc + (prod.valor || 0), 0);
-    setTotal(newTotal);
   };
 
   const addProduct = (nuevoProducto) => {
@@ -44,16 +123,11 @@ function App() {
       return producto;
     });
     setProduct(updatedProducts);
-    const newTotal = updatedProducts.reduce(
-      (acc, prod) => acc + (prod.valor || 0),
-      0
-    );
-    setTotal(newTotal);
   };
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Lista de compras</h1>
+      <h1 className="text-3xl font-bold pb-2 ">Lista de compras</h1>
       <ComprasForm addTask={addProduct} />
       <ListarCompras
         product={product}
