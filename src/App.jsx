14263a975e124bfeb9bb031e3a0500aa2617b9bf
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-06-24 11:51:55 -0300

    se instala tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,7 @@
-// import "./App.css";
-// import { useState } from "react";
-// import ListarCompras from "./components/ListaCompras";
-// import ComprasForm from "./components/ComprasForm";
-
-// function App() {
-//   const [product, setProduct] = useState([]);
-
-//   const onCompleted = (id) => {
-//     setProduct(
-//       product.map((producto) =>
-//         producto.id === Number(id)
-//           ? { ...producto, completed: !producto.completed }
-//           : producto
-//       )
-//     );
-//   };
-
-//   const onDeleteProduct = (id) => {
-//     setProduct(product.filter((producto) => producto.id !== id));
-//   };
-
-//   const addProduct = (nuevoProducto) => {
-//     const newProduct = {
-//       id: +new Date(),
-//       producto: nuevoProducto,
-//       completed: false,
-//     };
-//     setProduct([...product, newProduct]);
-//   };
-
-//   return (
-//     <>
-//       <h1>Lista de compras</h1>
-//       <ComprasForm addTask={addProduct} />
-//       <ListarCompras
-//         product={product}
-//         onCompleted={onCompleted}
-//         onDeleteProduct={onDeleteProduct}
-//       />
-//     </>
-//   );
-// }
-
-// export default App;
 import "./App.css";
 import { useState } from "react";
 import ListarCompras from "./components/ListaCompras";
 import ComprasForm from "./components/ComprasForm";
-import Producto from "./components/Producto"; // Asegúrate de que la ruta sea correcta
 
 function App() {
   const [product, setProduct] = useState([]);
@@ -99,7 +53,7 @@ function App() {
 
   return (
     <>
-      <h1>Lista de compras</h1>
+      <h1 className="text-3xl font-bold underline">Lista de compras</h1>
       <ComprasForm addTask={addProduct} />
       <ListarCompras
         product={product}
